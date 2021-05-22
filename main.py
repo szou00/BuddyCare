5dc13934143174ead6341328c22e70d2e0cc654b
Sharon Zou <43053716+szou00@users.noreply.github.com>
2021-05-21 20:59:35 -0400

    pages working and login works

--- main.py
+++ main.py
@@ -1,10 +1,80 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash, g
 from datetime import timedelta
 
 from flask.signals import Namespace
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "weAreCoolest"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(days=7)
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+class User():
+
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='Tyler', password="iscoolest"))
+users.append(User(id=2, username='Fiona', password="iscool"))
+users.append(User(id=3, username='Sharon', password="issomewhatcool"))
+
+
+
+@app.route("/")
+@app.route('/home')
+def home():
+    return render_template("index.html")
+    
+@app.route("/profile")
+def profile():
+        return render_template('profile.html')
+
+@app.route("/activity")
+def activity():
+    return render_template("activity.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        #db 
+        user = [x for x in users if x.username == username][0]
+        #print(user)
+        #print(user[0])
+        if user and user.password == password:
+            session['user_id'] = user.id
+            flash("Login Successful!")
+            return redirect(url_for('profile'))
+        else:
+            flash("Login Unsuccessful :(")
+            return redirect(url_for("login"))
+    else: 
+        if "user" in session:
+            flash("Already Logged In!")
+            return redirect(url_for("home"))
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out!", "info")
+    session.pop("user", None)
+    #session.pop("email", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
