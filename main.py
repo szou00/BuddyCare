1a109760adad04ba405df1de9942f17bba8d8280
Sharon Zou <43053716+szou00@users.noreply.github.com>
2021-05-23 01:13:38 -0400

    mostly working, just fixing up user interface

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+import secrets
+from PIL import Image
 from flask import Flask, redirect, url_for, render_template, request, session, flash, g, Response
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, date
 from werkzeug.utils import secure_filename
+import time
 
 from flask.signals import Namespace
 from flask_sqlalchemy import SQLAlchemy
@@ -13,6 +17,8 @@ app.permanent_session_lifetime = timedelta(days=7)
 
 db = SQLAlchemy(app)
 db1 = SQLAlchemy(app)
+# db2 = SQLAlchemy(app)
+
 class User(db.Model):
     id = db.Column("id", db.Integer, primary_key= True)
     username = db.Column("username", db.String(100))
@@ -24,9 +30,9 @@ class User(db.Model):
     achieve = db.Column("achieve", db.String(300))
     joke = db.Column("joke", db.String(200))
     budName = db.Column("buddy",db.String(200))
-    #image_file = db.Column("img", db.String(100), default="sunflowers.jpg")
-    #mimetype = db.Column("mimetyle", db.Text)
-
+    image_file = db.Column("img", db.String(100), default="sunflowers.jpg")
+    #lastCheckIn= db.Column(db.DateTime, default=datetime.utcnow)
+    
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
@@ -46,6 +52,7 @@ class User(db.Model):
 class newActivity(db1.Model):
      id1 = db1.Column("id1", db1.Integer, primary_key= True)
      activityname= db1.Column("activityname", db1.String(100))
+     description = db1.Column(db1.Text, default=("No Description"))
 
      def __init__(self,activityname):
          self.activityname=activityname
@@ -53,11 +60,35 @@ class newActivity(db1.Model):
      def __repr__(self):
          return self.activityname
 
+# class checkInTimes():
+#     id2=db2.Column("id2", db2.Integer, primary_key=True)
+#     timeCheckIn= db2.Column(db.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return self.timeCheckIn
+
 db.create_all()
 db.session.commit()
 
+
 db1.create_all()
 db1.session.commit()
+
+# db2.create_all()
+# db2.session.commit()
+
+# walk = newActivity("Walk")
+# walk.description = "Take a nice walk outside"
+# journal = newActivity("Journal")
+# journal.description = "Write down how you're feeling!"
+# meditation = newActivity("Meditation")
+# meditation.description = "Meditate for half an hour :)"
+
+# db1.session.add(walk)
+# db1.session.add(journal)
+# db1.session.add(meditation)
+# db1.session.commit()
+# i want to see if these can be the default ones
 
 @app.before_request
 def before_request():
@@ -65,19 +96,29 @@ def before_request():
     if 'name' in session and not(session['name'] == None):
         user = User.query.filter_by(username = session['name']).first()
         g.user = user
+    
 
 @app.route("/")
 @app.route('/home')
 def home():
-    if 'name' in session:
+    if User == None or len(User.query.all())==0:
+        flash("Last user was not properly logged out - logging out!")
+        return redirect("logout")
+    elif 'name' in session:
         found_user = User.query.filter_by(username = session['name']).first()
         if found_user:
             return render_template("index.html", values=User.query.all(),userFound=True,user=found_user)
+        else:
+            session.pop("name",None)
+            return render_template("index.html")
     else:
         return render_template("index.html", values=User.query.all(),userFound=False)
 
 @app.route('/<name>', methods=['POST','GET'])
 def viewBuddy(name):
+    if not 'name' in session:
+        flash("You need to login first!")
+        return render_template("index.html")
     buddy = User.query.filter_by(username=name).first()
     user = User.query.filter_by(username=session['name']).first()
     if buddy == None:
@@ -93,7 +134,7 @@ def viewBuddy(name):
     
 @app.route("/profile", methods=["POST", "GET"])
 def profile():
-
+    
     #the profile image part after adding new column dont delete
     #image_file = url_for('static', filename='images/' + User.image_file)
 
@@ -107,7 +148,7 @@ def profile():
         #print("it works at this line")
         print(request.method)
         if request.method=="POST":
-            if request.form['button'] == 'removeBuddy':
+            if request.form['button'] == 'Remove Buddy':
                 flash("removing :((")
                 budName = user.budName
                 buddy = User.query.filter_by(username=budName).first()
@@ -117,43 +158,80 @@ def profile():
                 buddy.bud = True
                 db.session.commit()
                 return render_template('profile.html', user=user)
-            elif request.form["button"]== "checkin":
+            elif request.form["button"]== "Check In":
                 if 'name' in session:
                     user = User.query.filter_by(username=session['name']).first()
+                    # if user.streak==0:
+                    #     user.lastCheckIn= datetime.now()
+                    #     user.streak=1
+                    # else:
+                    #     now= datetime.now()
+                    #     last= user.lastCheckIn
+                    #     print(now)
+                    #     print(last)
+                    #     if not (now<last or now==last):
+                    #         user.streak= user.streak+1
+                    #         user.lastCheckIn=datetime.now()
                     user.streak= user.streak+1
                     db.session.commit()
-                return render_template('profile.html',user=user)
+                return render_template('profile.html', user=user)
             else:
                 bio = request.form["bio"]
                 achieve = request.form["achieve"]
                 joke = request.form["joke"]
-                
-                if not(bio == None):
+                pfp = request.files["pfp"]
+
+                if bio:
                     user.bio = bio
-                if not(achieve == None):
+                if achieve:
                     user.achieve = achieve
-                if not(joke == None):
+                if joke:
                     user.joke = joke
+                if pfp:
+                    flash("Profile Successfully Changed")
+                    # print("AJDSNAJDFSAJFSAJFNADKJA")
+                    #filename = secure_filename(pfp.filename)
+                    picture_file = save_picture(pfp)
+                    user.image_file = picture_file
+                    
                 db.session.commit()
-        elif request.method == 'GET':
+                return render_template('profile.html', user=user)
+        else:
             return render_template('profile.html', user=user)
     else:
         flash("You must login first!")
         return redirect(url_for("login"))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+
+    output_size = (200,200)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
 @app.route("/activity", methods=["POST", "GET"])
 def activity():
     if request.method == "POST":
         activity=request.form["activity"]
+        description=request.form["description"]
+        
         find_act=newActivity.query.filter_by(activityname=activity).first()
         if find_act:
             flash("activity already exists")
             session["activityname"] = find_act.activityname
         else:
             actname = newActivity(activity)
+            if description:
+                actname.description = description
             flash("new activity added")
             db1.session.add(actname)
             db1.session.commit()
+            
     if (newActivity.query.first == None):
         return render_template("activity.html")
     return render_template("activity.html",activities=newActivity.query.all())
@@ -209,8 +287,11 @@ def login():
             session['name'] = user.username
             flash("Login Successful!")
             return redirect(url_for('profile'))
+        elif user:
+            flash("Wrong password")
+            return redirect(url_for("login"))
         else:
-            flash("Login Unsuccessful :(")
+            flash("No account with that username exists. Please create a new acc!")
             return redirect(url_for("login"))
     else: 
         if "name" in session:        
@@ -246,11 +327,11 @@ def questions():
     if request.method == "POST":
         if "name" in session:
             session.permanent = True
-            #
-            user.bio=request.form["bio"]
-            user.achieve=request.form["achieve"]
-            user.joke=request.form["joke"]
-            db.session.commit()
+
+            # user.bio=request.form["bio"]
+            # user.achieve=request.form["achieve"]
+            # user.joke=request.form["joke"]
+            # db.session.commit()
             return redirect(url_for("profile"))
     return render_template("Questions.html", user=user)
 
@@ -284,6 +365,9 @@ def addActivity():
 
 @app.route("/activity/<activityName>", methods=["POST", "GET"])
 def activityPage(activityName):
+    
+    activity = newActivity.query.filter_by(activityname=activityName).first()
+    
     if request.method == "POST":
         if "name" in session:
             flash("yay!")
@@ -291,7 +375,8 @@ def activityPage(activityName):
             user.activity = activityName
             db.session.commit()
             return redirect(url_for("profile"))
-    return render_template("chooseActivity.html", name=activityName) 
+        
+    return render_template("chooseActivity.html", act=activity) 
 
 
 
