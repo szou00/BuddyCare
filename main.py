72cd386a1d7fdf55db28ecc6ebb5e2b5054285c0
Sharon Zou <43053716+szou00@users.noreply.github.com>
2021-07-11 17:51:58 -0400

    yeah im back at it l o l

--- main.py
+++ main.py
@@ -392,6 +392,6 @@ def logout():
     return redirect(url_for("login"))
 
  
-if __name__ == "__main__":
-    # db.create_all()
-    app.run(debug=True) 
+# if __name__ == "__main__":
+#     # db.create_all()
+#     app.run(debug=True) 
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from main import app
+if __name__ == "__main__":
+    # db.create_all()
+    app.run(debug=True) 
