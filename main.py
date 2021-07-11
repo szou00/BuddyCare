3214f44faa3b3074d20173f699cbe442a60019c7
Sharon Zou <43053716+szou00@users.noreply.github.com>
2021-07-11 18:16:15 -0400

    changed port to fix binding issue

--- main.py
+++ main.py
@@ -392,6 +392,7 @@ def logout():
     return redirect(url_for("login"))
 
  
-# if __name__ == "__main__":
-#     # db.create_all()
-#     app.run(debug=True) 
+if __name__ == "__main__":
+    # db.create_all()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=True) 
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from main import app
-if __name__ == "__main__":
-    # db.create_all()
-    app.run(debug=True) 
